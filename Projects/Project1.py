d1babc2041bf1cd473769926da35d33685ea5dc3
Curtis Castro <curtisncastro@gmail.com>
2021-08-30 20:09:40 -0700

    complete project 1; drawing with markers

--- Projects/Project1.py
+++ Projects/Project1.py
@@ -11,21 +11,29 @@ cap.set(10, 150) # Set brightness 100% (the first argument is the setting ID num
 myColors = [[5, 107, 0, 19, 255, 255],
             [133, 56, 0, 159, 156, 255],
             [57, 76, 0, 100, 255, 255]]
+
 myColorValues = [[51,153,255],
                  [255,0,255],
                  [0,255,0]]
 
+myPoints = [] ## [x,y,colorId]
+
 def findColor(img,myColors,myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     count = 0
+    newPoints = []
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
         x,y = getContours(mask)
-        cv2.circle(imgResult,(x,y),10,(255,0,0),cv2.FILLED)
+        cv2.circle(imgResult,(x,y),15,myColorValues[count],cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x,y,count])
         count += 1
         ## cv2.imshow(str(color[0]), mask)
+    return newPoints
+
 
 def getContours(img):
     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
@@ -39,12 +47,21 @@ def getContours(img):
             x, y, w, h = cv2.boundingRect(approx)
     return x+w//2,y
 
+def drawOnCanvas(myPoints,myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult,(point[0],point[1]),10,myColorValues[point[2]],cv2.FILLED)
+
 while True:
     success, img = cap.read()
     if img is None:
         break
     imgResult = img.copy()
-    findColor(img,myColors,myColorValues)
+    newPoints = findColor(img,myColors,myColorValues)
+    if len(newPoints) != 0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints) != 0:
+        drawOnCanvas(myPoints,myColorValues)
     cv2.imshow("Video", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
